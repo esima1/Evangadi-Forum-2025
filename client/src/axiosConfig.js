b6c1a552ec20a6fcc3c1398c25eab736696a06f8
esima1 <mokemebratu@gmail.com>
2025-01-04 13:43:36 -0600

    evangadi2

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
  const token = localStorage.getItem("token");
 const axiosBase = axios.create({
-  baseURL: "http://localhost:3306/api", 
+  // baseURL: "http://localhost:3306/api"
+  baseURL: "https://evangadi-forum-2025.onrender.com/api",
 });
 
 export default axiosBase;
